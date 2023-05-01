a628b1f4a2d2fb1e0f65f8113776c2f941da7419
t <82817414+Derek46518@users.noreply.github.com>
2023-05-02 00:57:44 +0800

    Initialize Mission

--- DS04/DS04_11_11027205_11027229.cpp
+++ DS04/DS04_11_11027205_11027229.cpp
@@ -18,6 +18,7 @@
 #include <cstdlib>
 #include <cmath>
 #include <iomanip>
+#include <algorithm>
 
 using namespace std;
 
@@ -26,6 +27,84 @@ struct Data
     char putID[10] = {'\0'};
     char getID[10] = {'\0'};
     float weight;
+};
+
+
+
+class Node{
+    public:
+        Node(string id,string put, float weight){
+            currentID = put;
+            pairs.push_back(make_pair(id,weight));
+        }
+
+        void addPair(string pairp, float weight){
+            pair<string,float> temp = make_pair(pairp,weight);
+            pairs.push_back(temp);
+        }
+
+        void sort(){
+            std::sort(pairs.begin(), pairs.end());
+        }
+        
+        void printPairID(){
+            for(pair<string,float> p : pairs){
+                cout << p.first << ' ' << p.second <<'\t';
+            }
+            cout << '\n';
+        }
+        string currentID;
+        
+    private:
+        
+        vector<pair<string,float> > pairs;
+};
+
+
+
+class Graph{
+    public:
+        void clear(){
+            graph.clear();
+        }
+        
+        bool findAndInsert(string getID, string putID, float weight){
+            for(Node &node : graph){
+                if(node.currentID==putID){
+                    
+                    node.addPair(getID,weight);
+                    
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        
+
+        void sort(){
+            std::sort(graph.begin(),graph.end(),[](const Node& s1, const Node& s2){return s1.currentID<s2.currentID;});
+            for(Node node : graph){
+                node.sort();
+            }
+        }
+        void createGraph(vector<Data> data){
+            for(Data d : data){
+                if(!findAndInsert(d.getID,d.putID,d.weight)){
+                    graph.push_back( Node(d.getID,d.putID,d.weight) );
+                }
+            }
+        }
+        void printAll(){
+            for(Node n : graph){
+                cout << n.currentID << '\n';
+                n.printPairID();
+            }
+        }
+
+    private:
+        vector<Node> graph;
+        
 };
 
 //  write binary to struct
@@ -50,13 +129,38 @@ void WriteToVec(string binFilename, vector<Data> &data)
     binFile.close();
 } // void WriteToVec
 
+
+
+int getInt()
+{
+  // 先讀整行
+  string s;
+  getline(cin, s);
+  int total = 0; // total
+  int a;         // 暫存的數字
+  if (s.length() == 0)
+    return -1; // 如果使用者直接按下enter 就回傳-1(執行main時會報使用者輸入錯誤)
+  // string to integer
+  for (int i = 0; i < s.length(); i++)
+  {
+    total *= 10;
+    a = s[i] - '0';
+    if (a < 0 || a > 9)
+      return -1; // 如果不在範圍內 就回傳-1 (執行main時會報使用者輸入錯誤)
+    total += a;
+  }
+  return total; // 將值回傳回去
+}
+
 int main()
 {
     vector<Data> data;
+    Graph graph;
     WriteToVec("pairs401.bin",data);
     for(Data d : data){
-        cout << d.putID << '\n';
-        cout << d.getID << '\n';
-        cout << d.weight <<'\n';
+        cout << d.getID << ' ' << d.putID << ' ' << d.weight << '\n';
     }
+    graph.createGraph(data);
+    graph.sort();
+    graph.printAll();
 }
