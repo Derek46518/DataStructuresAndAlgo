7a5c3dfb8070d97976a52276268e605c688e075d
NierWL <kpo852200@gmail.com>
2023-05-02 15:48:11 +0800

    update mission 1 code

--- DS04/DS04_11_11027205_11027229.cpp
+++ DS04/DS04_11_11027205_11027229.cpp
@@ -1,7 +1,7 @@
 /**
  * @file DS04_11_11027205_11027229.cpp
  * @author Derek(11027229) and Nier(11027205)
- * @brief 這是一個被搞到的專案 用來解決作業4
+ * @brief ?�是一?�被?�到?��?�??��?�?��作業4
  *
  * @version 1.0
  * @date 2023-04-21
@@ -48,7 +48,7 @@ class Node{
         
         void printNodes(){
             for(auto p : pairs){
-                cout << p.first ->currentID << '\t';
+                cout << p.first ->currentID << '\t' << p.second << '\t';
             }
             cout << '\n';
         }
@@ -79,14 +79,14 @@ class Graph{
         
         void createGraph(vector<Data> data){
             for(Data d : data){
-                Node * firstPair = find(d.getID);
-                Node * secondPair = find(d.putID);
+                Node * firstPair = find(d.putID);
+                Node * secondPair = find(d.getID);
                 if(firstPair==NULL) {
-                    firstPair = new Node(d.getID);
+                    firstPair = new Node(d.putID);
                     graph.push_back(firstPair);
                 }
                 if(secondPair==NULL) {
-                    secondPair = new Node(d.putID);
+                    secondPair = new Node(d.getID);
                     graph.push_back(secondPair);
                 }
                 firstPair->addPair(secondPair,d.weight);
@@ -134,23 +134,23 @@ void WriteToVec(string binFilename, vector<Data> &data)
 
 int getInt()
 {
-  // 先讀整行
+  // ?��??��?
   string s;
   getline(cin, s);
   int total = 0; // total
-  int a;         // 暫存的數字
+  int a;         // ?��??�數�?
   if (s.length() == 0)
-    return -1; // 如果使用者直接按下enter 就回傳-1(執行main時會報使用者輸入錯誤)
+    return -1; // 如�?使用?�直?��?下enter 就�???1(?��?main?��??�使?�者輸?�錯�?
   // string to integer
   for (int i = 0; i < s.length(); i++)
   {
     total *= 10;
     a = s[i] - '0';
     if (a < 0 || a > 9)
-      return -1; // 如果不在範圍內 就回傳-1 (執行main時會報使用者輸入錯誤)
+      return -1; // 如�?不在範�???就�???1 (?��?main?��??�使?�者輸?�錯�?
     total += a;
   }
-  return total; // 將值回傳回去
+  return total; // 將值�??��???
 }
 
 int main()
@@ -159,7 +159,7 @@ int main()
     Graph graph;
     WriteToVec("pairs401.bin",data);
     for(Data d : data){
-        cout << d.getID << ' ' << d.putID << ' ' << d.weight << '\n';
+        cout << d.putID << ' ' << d.getID << ' ' << d.weight << '\n';
     }
     graph.createGraph(data);
     graph.printAll();
