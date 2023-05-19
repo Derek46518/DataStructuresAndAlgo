35fb8c0659ea0411ded9378f9f04d1490b7c96cf
NierWL <kpo852200@gmail.com>
2023-05-19 19:24:37 +0800

    update code
    finish  mission2

--- DS05/DS05_11_11027205_11027229.cpp
+++ DS05/DS05_11_11027205_11027229.cpp
@@ -93,6 +93,11 @@ class File {
 			fileName = fileName.erase( fileName.size()-4, 4 ) + ".cc" ;
 			return fileName ;
 		} // void Create_adjFile
+		
+		string Create_dsFile( string fileName ) {
+			fileName = fileName.erase( fileName.size()-4, 4 ) + ".ds" ;
+			return fileName ;
+		} // void Create_adjFile
 }; // class File
 
 class Graph  {
@@ -110,6 +115,17 @@ class Graph  {
 			return (int)sender.size() ;
 		} // int getNodeTotal
 		
+		int findList( char id[10], vector<Sender> &list ) {
+			int position = 0 ;
+			for ( int i=0; i < list.size(); i++ )  {
+				if ( strcmp( id, list[i].id ) == 0 ) {
+					position = i ;
+					break ;
+				} // if
+			} // for
+			return position ;
+		} // int findList
+		 
 		// @ brief : init temp list struct
 		void InitST( Sender &sender, Receiver &receiver ) {
 			sender.id[10] = {'\0'} ;
@@ -150,6 +166,23 @@ class Graph  {
 						if ( strcmp( connectList[i].receiver[j].id, connectList[i].receiver[k].id ) > 0 )
 							swap(  connectList[i].receiver[j],  connectList[i].receiver[k] ) ;
 					}
+				} // for
+			} // for
+		} // void SortMission1
+		
+		void SortMission2( Sender &dsArray ) {
+			for ( int i=0; i < dsArray.receiver.size(); i++ ) {
+				for ( int j=i; j < dsArray.receiver.size(); j++ ) {
+					if ( dsArray.receiver[i].weight >= dsArray.receiver[j].weight ) {
+						if ( dsArray.receiver[i].weight > dsArray.receiver[j].weight )  {
+							swap ( dsArray.receiver[i], dsArray.receiver[j] ) ;
+						} // if
+						else {
+							if ( strcmp( dsArray.receiver[i].id, dsArray.receiver[j].id ) > 0 )  {
+								swap ( dsArray.receiver[i], dsArray.receiver[j] ) ;
+							} // if
+						} // else
+					} // if
 				} // for
 			} // for
 		} // void SortMission1
@@ -234,6 +267,25 @@ class Graph  {
 
 			ccFile.close() ;
 		} //  void WriteListToCc
+		
+		void WriteListToDs( string dsFileName, Sender dsArray ) {
+			ofstream dsFile ;
+			int count = 0 ;
+			
+			dsFile.open( dsFileName.c_str(), std::ofstream::out | std::ofstream::app ) ;
+			dsFile << "\norigin: " << dsArray.id << "\n" ;
+			for ( int i=0; i < dsArray.receiver.size(); i++ ) {
+				count++ ;
+				dsFile << "(" << i+1 << ") \t" << dsArray.receiver[i].id << ", " << dsArray.receiver[i].weight << "\t"   ;
+				if ( count == 8 ) {
+					dsFile << "\n" ;
+					count = 0 ;
+				} // if
+			}  // for
+			 
+			dsFile << "\n" ;
+			dsFile.close() ;
+		} // void
 		
 		// @ brief :  build graph
 		void BuildList( vector<Sender> &list, vector<Data> data ) {
@@ -333,7 +385,7 @@ class Graph  {
 				if ( IsVisit( list[i].id, visit ) == false ) {
 					stack.push_back( list[i].id ) ;
 					visit.push_back( list[i].id ) ;
-					traver( list[i].id, stack, visit, list, tempSender ) ;
+					traveler( list[i].id, stack, visit, list, tempSender ) ;
 					connectList.push_back(tempSender) ;
 					tempSender.receiver.clear() ;
 					stack.clear() ;
@@ -343,7 +395,46 @@ class Graph  {
 			SortMission1( connectList ) ;
 		} // void BuildConnectList
 		
-		void traver( string id, vector<string> &stack, vector<string> &visit, vector<Sender> list, Sender &tempSender ) {
+		void  BuildDs( string id, vector<Sender> &dsArray, vector<Sender> list, vector<Sender> connectList ) {
+			Sender tempSender ;
+			int position = 0 ;
+			
+			// find ID
+			for ( int i=0; i < connectList.size(); i++ ) {
+				for ( int j=0; j < connectList[i].receiver.size(); j++ ) {
+					if ( id == connectList[i].receiver[j].id ) {
+						position  = i ;
+						break ;
+					} // if
+				} // for
+			} // for
+			
+			// set start & finish
+			for ( int i=0; i < connectList[position].receiver.size(); i++ ) {
+				strcpy ( tempSender.id, connectList[position].receiver[i].id ) ;
+				for ( int j=0 ; j < connectList[position].receiver.size(); j++ ) {
+					tempSender.receiver.push_back( connectList[position].receiver[j] ) ;
+				} //  for
+				dsArray.push_back(tempSender) ;
+				tempSender.id[10] = {'\0'} ;
+				tempSender.receiver.clear() ;
+			} // for
+			
+			// set weight
+			for ( int i=0; i < dsArray.size(); i++ ) {
+				position = findList( dsArray[i].id, list ) ;
+				for ( int j=0 ; j < list[position].receiver.size(); j++ ) {
+					for ( int k=0 ; k < dsArray[i].receiver.size(); k++ ) {
+						if ( strcmp ( list[position].receiver[j].id, dsArray[i].receiver[k].id ) == 0 ) {
+							dsArray[i].receiver[k].weight =  list[position].receiver[j].weight ;
+							break ;
+						} // if
+					} //  for
+				} // for
+			} // for
+		} // void BuildDijkstra
+		
+		void traveler( string id, vector<string> &stack, vector<string> &visit, vector<Sender> list, Sender &tempSender ) {
 			int position = -1 ;
 			int tempStack = stack.size() ;
 			Receiver tempReceiver ;
@@ -361,7 +452,7 @@ class Graph  {
 			if ( IsVisit( stack[stack.size()-1], visit ) == false ) {
 				visit.push_back( stack[stack.size()-1] ) ;
 				while ( !stack.empty() ) {
-					traver( stack[stack.size()-1], stack, visit, list, tempSender ) ;
+					traveler( stack[stack.size()-1], stack, visit, list, tempSender ) ;
 				} // while
 			} // if
 
@@ -378,7 +469,7 @@ class Graph  {
 		
 		void Add( vector<string> &stack, vector<string> &visit, Sender sender ) {
 			for ( int i=0; i < sender.receiver.size(); i++ ) {
-				if ( IsInStack( sender.receiver[i].id, stack ) == false && IsVisit( sender.receiver[i].id, visit ) == false  )
+				if ( InStack( sender.receiver[i].id, stack ) == false && IsVisit( sender.receiver[i].id, visit ) == false  )
 					stack.push_back( sender.receiver[i].id ) ;
 			} // for
 			
@@ -391,12 +482,178 @@ class Graph  {
 			return false ;
 		} // bool IsVisit
 		
-		bool IsInStack( string id, vector<string> stack ) {
+		bool InStack( string id, vector<string> stack ) {
 			for ( string s : stack ) {
 				if ( id == s ) return true ;
 			} //  for
 			return false ;
 		} // bool IsInStack
+		
+		bool InList( string id, vector<Sender> list ) {
+			for ( Sender l : list ) {
+				if ( id == l.id ) return true ;
+			} //  for
+			return false ;
+		} // bool InList
+		
+		int FindAnsArray( char id[10], Sender ansArray ) {
+			int position = 0 ;
+			for ( int i=0; i < ansArray.receiver.size(); i++ ) {
+				if ( strcmp ( id, ansArray.receiver[i].id ) == 0 ) {
+					position = i ;
+					break ;
+				} // if
+			} // for
+			 
+			return position ;
+		} // int FindAnsArray
+		
+		void ShortPath( string fileName, vector<Sender> list, vector<Sender> connectList ) {
+			File file ;
+			string tempID = "\0" ;
+			Sender ansArray ;
+			vector<Sender> dsArray ;
+			
+			fileName = file.Create_dsFile( fileName ) ;
+			
+			do {
+				Mission2_Print( list ) ;
+				cout << "\nInput a student ID [0: exit] : " ;
+				cin >> tempID ;
+				if ( tempID == "0" ) return ;
+				
+				if ( InList( tempID, list ) == true ) {
+					BuildDs( tempID, dsArray, list, connectList ) ;
+					dijkstra( tempID, ansArray, dsArray ) ;
+					SortMission2( ansArray ) ;
+					WriteListToDs( fileName, ansArray ) ;
+					ansArray.id[10] = {'\0'} ;
+					ansArray.receiver.clear() ;
+				} // if
+				
+				else {
+					cout << "\n### the student id does not exist! ###\n" ;
+				} // else
+			} while ( tempID != "0" ) ;
+		} // void ShortPath
+		
+		void dijkstra( string id, Sender &ansArray, vector<Sender> &dsArray ) {
+			 vector<string> visit ;
+			 int position = 0, tempPos = 0 ;
+			 float shortWeight = 999, tempMin = 999 ;
+			 float singleWeight = 0.0 ;
+			 string prePath = "\0" ;
+			 char tempID[10] = {'\0'} ;
+			 char startID[10] = {'\0'} ;
+			
+			// copy ID to ansArray
+	        for( int i=0; i < id.length(); i++ ) {
+	           	ansArray.id[i] = id[i] ;
+			} // for
+			
+			// find ID in dsArray
+			for ( int i=0; i < dsArray.size(); i++ ) {
+				if ( id == dsArray[i].id ) {
+					position  = i ;
+					break ;
+				} // if
+			} // for
+		
+			//  set ansArray
+			for ( int i=0; i < dsArray[position].receiver.size(); i++  ) {
+				if ( id != dsArray[position].receiver[i].id ) 
+					ansArray.receiver.push_back( dsArray[position].receiver[i] ) ;
+			} // for
+			
+
+			
+			// find smallest weight
+			for ( int i=0; i < dsArray[position].receiver.size(); i++ ) {
+			   	strcpy ( tempID, dsArray[position].receiver[i].id ) ;
+			    singleWeight = dsArray[position].receiver[i].weight ;
+			    if ( IsVisit( tempID, visit ) == false && singleWeight != 0 && singleWeight <= shortWeight ) {
+			    	startID[10] = {'\0'}  ;
+			    	strcpy( startID, tempID ) ;
+			    	shortWeight = singleWeight ;
+				} // if
+			} // for
+
+			position = 0 ;
+			visit.push_back( id ) ;
+			visit.push_back( startID ) ;
+			prePath = startID ;
+			
+			for ( int count=0; count < ansArray.receiver.size()-1; count++  ) {
+				
+				// find ID
+				for ( int i=0; i < dsArray.size(); i++ ) {
+					if ( dsArray[i].id == prePath ) {
+						position  = i ;
+						break ;
+					} // if
+			    } // for
+
+				tempID[10] = {'\0'} ;
+				singleWeight = 0.0 ;
+
+				
+				// put ID in nextPath
+			    for ( int i=0; i < dsArray[position].receiver.size(); i++ ) {
+			    	strcpy ( tempID, dsArray[position].receiver[i].id ) ;
+			    	singleWeight = dsArray[position].receiver[i].weight ;
+			    	
+			    	if ( IsVisit( tempID, visit ) == false && singleWeight != 0 ) {
+			    		
+			    		// find min weight
+			    		if ( singleWeight < tempMin ) {
+							tempMin = singleWeight ;
+			    			prePath = tempID ;
+						} // if
+						
+						singleWeight = singleWeight + shortWeight ;
+			    		
+			    		// find ansArray ID
+						for ( int temp=0; temp < ansArray.receiver.size(); temp++ ) {
+							if ( strcmp ( tempID, ansArray.receiver[temp].id ) == 0  ) {
+								tempPos  = temp ;
+								break ;
+							} // if
+						} // for
+						
+			    		if ( singleWeight < ansArray.receiver[tempPos].weight || ansArray.receiver[tempPos].weight == 0 ) {
+			    			ansArray.receiver[tempPos].weight = singleWeight ;
+						} // if
+
+					} // if
+				} // for
+				
+
+				tempMin = 999 ;
+				
+				// find smallest weight
+				for ( int i=0; i < ansArray.receiver.size(); i++ ) {
+					tempID[10] = {'\0'} ;
+			  	 	strcpy ( tempID, ansArray.receiver[i].id ) ;
+			    	singleWeight = ansArray.receiver[i].weight ;
+			    	if ( IsVisit( tempID, visit ) == false && singleWeight != 0 && singleWeight < tempMin ) {
+			    		//startID[10] = {'\0'} ;
+			    		//strcpy( startID, tempID ) ;
+			    		prePath = tempID ;
+			    		tempMin = singleWeight ;
+					} // if
+				} // for
+				
+				
+				visit.push_back( prePath ) ;
+				shortWeight = tempMin ;
+		
+				position = 0 ;
+				tempID[10] = {'\0'} ;
+				singleWeight = 0 ;
+				tempPos = 0 ;
+				tempMin = 999 ;
+			} // for
+		} // void dijkstra
 		
 		void Mission0_Print( vector<Sender> list ) {
 			int node = getTotalNode(list) ;
@@ -411,7 +668,21 @@ class Graph  {
 			for ( int i=ccSize-1; i >= 0; i-- ) {
 				cout << "{ " << i+1 << "} Connected Component: size = "  << connectList[i].receiver.size() << endl ;
 			} // for
-		} // void testPrint
+		} // void Mission1_Print
+		
+		void Mission2_Print( vector<Sender> list ) {
+			int count = 0 ;
+			cout << endl ;
+			for( Sender s : list ) {
+				cout << "   " << s.id ;
+				count++ ;
+				if ( count == 8 ) {
+					cout << "\n" ;
+					count = 0  ;
+				} // if
+			} // for
+			
+		} // void Mission2_Print
 		
 }; // class Graph
 
@@ -461,9 +732,9 @@ class Mission {
 				cout << "### "  <<  fileName << " does not exist! ###\n" ;
 		} // void Mission0
 		
-		void Mission1( string fileName, vector<Sender> list ) {
+		void Mission1( string fileName, vector<Sender> list, vector<Sender> &connectList ) {
 			Graph graph ;
-			vector<Sender> connectList ;
+			connectList.clear() ;
 			
 			if ( list.size() == 0 ) {
 				cout << "\n### There is no graph and choose 0 first. ###\n" ;
@@ -476,20 +747,20 @@ class Mission {
 			} // else 
 		} // void Mission1
 
-		void Mission2( string fileName, vector<Sender> list ) {
+		void Mission2( string fileName, vector<Sender> list, vector<Sender> connectList ) {
 			Graph graph ;
 			
 			if ( list.size() == 0 ) {
 				cout << "\n### There is no graph and choose 0 first. ###\n" ;
 			} // if
 			
+			else if ( connectList.size() == 0 )  {
+				cout << "\n### Choose 1 to find connected components. ###\n" ;
+			} // else if
+			
 			else  {
-				int node = graph.getTotalNode(list) ;
-				int id = graph.getTotalID(list) ;
-				cout << endl << fileName << endl ;
-				cout << "\n<<< There are " << id << " IDs in total. >>>\n" ;
-				cout << "\n<<< There are " << node << " nodes in total. >>>\n" ;
-			}
+				graph.ShortPath( fileName, list, connectList ) ;
+			} // else
 		} // void Mission2
 }; // class Mission
 
@@ -498,6 +769,7 @@ void Start() {
     string temp = "\0" ;
     string fileName = "\0" ;
     vector<Sender> list ;
+    vector<Sender> connectList ;
 	Mission mission ;
 	File file  ;
 	
@@ -519,10 +791,10 @@ void Start() {
 		  	    	mission.Mission0( fileName, list ) ;
 		  	    	break ;
 		   		case( 1 ) :
-		  	    	mission.Mission1( fileName, list ) ;
+		  	    	mission.Mission1( fileName, list, connectList ) ;
 		  	    	break ;
 		    	case( 2 ) :
-		  	    	mission.Mission2( fileName, list ) ;
+		  	    	mission.Mission2( fileName, list,  connectList ) ;
 		  	    	break ;
 		    	default :
 		  	    	cout << "\nexit" ;
